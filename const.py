f6dde438cede016118a947ae93e10e11fc5452bf
Tim Moran <tmoran@Angelas-MBP.localdomain>
2021-08-20 15:38:38 -0700

    Initial checkin
    Still need linting, cleanup

---
+++ const.py
@@ -0,0 +1,8 @@
+""" Constants for qmotion module
+"""
+
+# Long default, qsync can be slow. Can be overriden during initialization
+DEFAULT_TIMEOUT = 20
+TCP_PORT = 9760
+UDP_PORT = 9720
+BROADCAST_ADDRESS = "255.255.255.255"
